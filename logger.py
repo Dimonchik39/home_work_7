be1ffd7d937fe550974f5e78405fc292d20f34d6
Dimonchik <sudjadred@live.com>
2022-12-21 18:09:04 +0200

    add_logger_update1

--- logger.py
+++ logger.py
@@ -2,6 +2,9 @@ from datetime import datetime as dt
 
 
 def calc_logger(data):
+    '''
+    Функция записи логов в файл
+    '''
     time = dt.now().strftime('%d.%m.%Y.%H.%M')
     with open('cal_log.log', 'a') as file:
         file.write('\n{};Результат: {}'
